3315fd7f930b25663224a85ea55e6d245b91fd22
docmanna <martin@backyard.net>
2019-01-05 19:17:19 +0100

    project clean up

--- src/main/java/net/docmanna/minecraft/magicbar/MagicBarMod.java
+++ src/main/java/net/docmanna/minecraft/magicbar/MagicBarMod.java
@@ -1,13 +1,13 @@
 package net.docmanna.minecraft.magicbar;
 
-import net.minecraft.init.Blocks;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = MagicBarMod.MODID, name = MagicBarMod.NAME,
      version = MagicBarMod.VERSION)
